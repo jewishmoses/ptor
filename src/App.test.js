28208b3de242363f2dac46da357ed200523e3899
jewishmoses <work@xmoses.com>
2021-08-15 20:49:08 +0300

    updated README

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/הורדה/i);
   expect(linkElement).toBeInTheDocument();
 });
